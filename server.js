39b319a7b1e9476b8ff202a356688a7bc7982f6e
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-23 00:46:18 -0500

    db setup

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const { append } = require('express/lib/response');
-const { default: helmet } = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { dbConnect } = require("./dbConfig");
 
-require('dotenv').config();
+require("dotenv").config();
+dbConnect();
 const server = express();
-server.use(morgan('dev'))
+server.use(morgan("dev"));
 server.use(express.json());
-server.use(helmet())
+server.use(helmet());
 
-server.get('/', (req, res) => {
-    res.status(200).json({msg: 'Welcome to NodeJS server'})
+server.get("/", (req, res) => {
+  res.status(200).json({ msg: "Welcome to NodeJS server" });
 });
 
-server.post('/', (req, res) => {
-    const data = req.body;
+server.post("/", (req, res) => {
+  const data = req.body;
 
-    res.status(200).json({ msg: 'We got data', data: data})
-})
+  res.status(200).json({ msg: "We got data", data: data });
+});
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => `Node Server Listening on port: ${PORT}`)
+server.listen(PORT, () => `Node Server Listening on port: ${PORT}`);
