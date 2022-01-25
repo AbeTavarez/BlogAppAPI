6deeb4552db1485ca40433073b6a6c1966a864de
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-25 15:50:01 -0500

    jwt middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,23 @@
+const res = require("express/lib/response");
+const JWT = require("jsonwebtoken");
+
+const verifyJWT = () => {
+  let TOKEN = req.get("Authorization");
+
+  if (TOKEN === null || TOKEN === undefined) {
+    res.status(403).json({ message: "You must be logged in" });
+  }
+
+  JWT.verify(TOKEN, process.env.JWT_SECRET, (error, result) => {
+    if (error) {
+      res.status(400).json({ message: "Validation Error" });
+    }
+
+    if (result === false) {
+      res.status(403).json({ message: "You must be logged in" });
+    }
+    next();
+  });
+};
+
+module.exports = verifyJWT;
